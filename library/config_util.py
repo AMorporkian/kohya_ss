f9982b9eb782150d0599607cbe8435417dc73892
SargeZT <patrick.e.shanahan@gmail.com>
2023-05-19 10:42:07 -0500

    stashing

--- library/config_util.py
+++ library/config_util.py
@@ -450,7 +450,7 @@ def generate_dreambooth_subsets_config_by_subdirs(train_data_dir: Optional[str] 
     caption_by_folder = '_'.join(tokens[1:])
     return n_repeats, caption_by_folder
 
-  def generate(base_dir: Optional[str], is_reg: bool = False, is_val: bool = False):
+  def generate(base_dir: Optional[str], is_reg: bool = False):
     if base_dir is None:
       return []
 
@@ -467,7 +467,7 @@ def generate_dreambooth_subsets_config_by_subdirs(train_data_dir: Optional[str] 
       if num_repeats < 1:
         continue
 
-      subset_config = {"image_dir": str(subdir), "num_repeats": num_repeats, "is_reg": is_reg,  "is_val": is_val, "class_tokens": class_tokens}
+      subset_config = {"image_dir": str(subdir), "num_repeats": num_repeats, "is_reg": is_reg,  "class_tokens": class_tokens}
       subsets_config.append(subset_config)
 
     return subsets_config
